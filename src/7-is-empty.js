548df4c92eab3dc4cf3125f6480cf51001dfc05c
Tanmark <thereseanmark@hotmail.com>
2022-11-20 17:47:07 +0100

    answers

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -4,6 +4,13 @@ export const isEmpty = (stringArrayOrObject) => {
   if (type === 'string') {
     return stringArrayOrObject === ''
   }
-
-  return false
+  if (type === "array") {
+    return stringArrayOrObject.length === 0
+  } 
+  if (type === "object") {
+    return Object.entries(stringArrayOrObject).length === 0
+  }
+   else {
+    return false
+  }
 }
